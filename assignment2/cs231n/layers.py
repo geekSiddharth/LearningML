7c4e57cbc867edfdb715c17bd013959cf856b79c
geekSiddharth <siddharth16268@iiitd.ac.in>
2017-07-05 18:00:58 +0530

    affine_forward implemented

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -24,7 +24,12 @@ def affine_forward(x, w, b):
     # TODO: Implement the affine forward pass. Store the result in out. You   #
     # will need to reshape the input into rows.                               #
     ###########################################################################
-    pass
+
+    # x_row is N, (D=(d_1 * ... * d_k))
+    x_row = [y.flatten() for y in x]
+
+    out = np.dot(x_row, w) + b
+
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
